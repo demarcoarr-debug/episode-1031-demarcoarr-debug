f42a17f8e92eb5f26edd9a3e32fe3acbeb99a357
De'Marco Arrington <Demarcoarr@gmail.com>
2025-11-07 02:53:58 -0800

    This is the code that i have

--- pi-estimator.py
+++ pi-estimator.py
@@ -3,4 +3,17 @@ import random
 total_darts=8
 if len(sys.argv)>1:
     total_darts=int(sys.argv[1])
-print('hi')
+
+in_circle=0
+
+
+for i in range(total_darts):
+   dart_x= random.random()
+   dart_y=random.random()
+   if dart_x*dart_x + dart_y*dart_y<=1:
+       in_circle+=1
+
+print(f'We got {in_circle} out of {total_darts} inside the circle')
+
+print('our estimate for Π is' ,{4.0* in_circle/total_darts})
+
